1ff3007ab0f7ea66ea7a626822f4b5253f7cbc8d
ScriptYooo <ggleafx.mr@gmail.com>
2025-05-05 23:44:00 +0500

    Script.Hub.1.lua

--- Script.Hub.1.lua
+++ Script.Hub.1.lua
@@ -44,7 +44,7 @@ local function createButton(text, position)
 end
 
 -- Create Bond Button (Position at the top)
-local bondButton = createButton("By BlackGem", UDim2.new(0.5, -100, 0, 10))
+local bondButton = createButton("By GG_LeafX", UDim2.new(0.5, -100, 0, 10))
 
 -- Create additional buttons (1 to 10)
 local button1 = createButton("Fly", UDim2.new(0.5, -100, 0, 70))
