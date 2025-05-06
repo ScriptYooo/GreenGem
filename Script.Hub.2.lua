ac3afff68e4329092f6d7a4548a9483cd3b08b1e
ScriptYooo <ggleafx.mr@gmail.com>
2025-05-06 09:11:55 +0500

    Script.Hub.2.lua

--- Script.Hub.2.lua
+++ Script.Hub.2.lua
@@ -44,18 +44,18 @@ local function createButton(text, position)
 end
 
 -- Create Bond Button (Position at the top)
-local bondButton = createButton("By BlackGem", UDim2.new(0.5, -100, 0, 10))
+local bondButton = createButton("Page 2", UDim2.new(0.5, -100, 0, 10))
 
 -- Create additional buttons (1 to 10)
-local button1 = createButton("Fly", UDim2.new(0.5, -100, 0, 70))
-local button2 = createButton("Meteor", UDim2.new(0.5, -100, 0, 130))
-local button3 = createButton("INFyield", UDim2.new(0.5, -100, 0, 190))
-local button4 = createButton("Button 4", UDim2.new(0.5, -100, 0, 250))
-local button5 = createButton("Button 5", UDim2.new(0.5, -100, 0, 310))
-local button6 = createButton("Button 6", UDim2.new(0.5, -100, 0, 370))
-local button7 = createButton("Button 7", UDim2.new(0.5, -100, 0, 430))
-local button8 = createButton("Button 8", UDim2.new(0.5, -100, 0, 490))
-local button9 = createButton("Button 9", UDim2.new(0.5, -100, 0, 550))
+local button1 = createButton("Comming Soon", UDim2.new(0.5, -100, 0, 70))
+local button2 = createButton("comming soon", UDim2.new(0.5, -100, 0, 130))
+local button3 = createButton("Comming Soon", UDim2.new(0.5, -100, 0, 190))
+local button4 = createButton("Comming Soon", UDim2.new(0.5, -100, 0, 250))
+local button5 = createButton("Comming Soon", UDim2.new(0.5, -100, 0, 310))
+local button6 = createButton("Comming Soon", UDim2.new(0.5, -100, 0, 370))
+local button7 = createButton("Comming Soon", UDim2.new(0.5, -100, 0, 430))
+local button8 = createButton("Comming Soon", UDim2.new(0.5, -100, 0, 490))
+local button9 = createButton("Comming Soon", UDim2.new(0.5, -100, 0, 550))
 local button10 = createButton("More", UDim2.new(0.5, -100, 0, 610))
 
 -- Adjust Frame Height Based on Buttons
@@ -119,16 +119,14 @@ end)
 
 -- Additional button click functions (optional)
 button1.MouseButton1Click:Connect(function()
---ARCEUS X FLY V2 SCRIPT
-loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
-end)
 
+end)
 button2.MouseButton1Click:Connect(function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptBeLike/Tera-DeadRails/refs/heads/main/Meteor%20V1"))()
+
 end)
 
 button3.MouseButton1Click:Connect(function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+
 end)
 
 button4.MouseButton1Click:Connect(function()
